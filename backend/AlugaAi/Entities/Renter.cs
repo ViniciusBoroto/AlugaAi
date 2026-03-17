79c79c077cbac881a7f97df591d4a6df50a27e78
luaanHD <luantoniolo94@gmail.com>
2026-03-17 20:25:29 -0300

    feat: new backend

---
+++ backend/AlugaAi/Entities/Renter.cs
@@ -0,0 +1,10 @@
+﻿namespace AlugaAi.Entities
+{
+    public class Renter : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
