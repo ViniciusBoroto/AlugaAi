bfc2090468b89f91f05ccd65acf33c757ef176aa
luaanHD <luantoniolo94@gmail.com>
2026-03-20 20:28:29 -0300

    feat: entities Rent and Store

---
+++ backend/AlugaAi/Entities/Rent.cs
@@ -0,0 +1,14 @@
+﻿namespace AlugaAi.Entities
+{
+    public class Rent:BaseEntity
+    {
+        public DateTime RentalDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public DateTime? DeliveredAt { get; set; } = null;
+        public DateTime? ReturnedAt{ get; set; } = null;
+        public Product Product { get; set; }
+        public Guid ProductId { get; set; }
+        public Renter Renter { get; set; }
+        public Guid RenterId { get; set; }
+    }
+}
---
+++ backend/AlugaAi/Entities/Store.cs
@@ -0,0 +1,14 @@
+﻿namespace AlugaAi.Entities
+{
+    public class Store:BaseEntity
+    {
+        public string FantasyName { get; set; }
+        public string Cnpj { get; set; }
+        public string Adress { get; set; }
+        public string CEP { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+        public string PasswordHash { get; set; }
+    }
+}
